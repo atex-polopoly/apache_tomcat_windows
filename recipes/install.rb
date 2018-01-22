8f1b5cbbe72a9c8e053673ce46f73cfa4c075740
David Wood <dwood@atex.com>
2018-01-22 10:34:18 +0000

    MS-139: Couple of corrections to recent commit

--- recipes/install.rb
+++ recipes/install.rb
@@ -11,12 +11,14 @@ chef_gem 'win32-dir'
 
 require 'win32/dir'
 
+tomcat_folder = "#{Dir::PROGRAM_FILES}\\#{node[:tomcat][:win_install_dir]}"
+
 tomcat_download = remote_file 'apache tomcat' do
   path "#{Chef::Config[:file_cache_path]}\\apache_tomcat.exe"
-  source {"ftp://#{node[:ftp][:host]}#{node[:ftp][:path]}/apache_tomcat/apache-tomcat-#{node[:tomcat][:version]}.exe"}
+  source "ftp://#{node[:ftp][:host]}#{node[:ftp][:path]}/apache_tomcat/apache-tomcat-#{node[:tomcat][:version]}.exe"
   ftp_active_mode node['ftp_active_mode']
   action :create
-  not_if {Dir::exist?("#{Dir::PROGRAM_FILES}\\#{node[:tomcat][:win_install_dir]}")}
+  not_if {Dir::exist?(tomcat_folder)}
 end
 
 # Install Tomcat (auto-searches for installed Java)
